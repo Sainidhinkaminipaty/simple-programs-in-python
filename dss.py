67f9113f4ccd2ef37982f0e9d3e89297d26c8255
roopsai <roopsai.work@gmail.com>
2018-06-24 17:00:54 +0200

    Create dss.py

---
+++ dss.py
@@ -0,0 +1,21 @@
+'''
+
+
+'''
+prob_dict = {'a':'1','b':'2','c':'3','d':'4','e':'5','f':'6'}
+print("city = S")
+while True:
+    problem = list(map(str,input("enter problems").split()))
+    if set(problem)&set(prob_dict.keys()):
+        break
+    else:
+        print("oops,give alphabets as input")
+solution = ['7']#common solution 
+for i in problem:
+    solution += prob_dict[i]
+    
+solution_int = []
+solution_int += [int(x) for x in solution]
+print(sorted(solution_int))
+        
+
